eeb72bad3df3fcc95578208db98f94c7c2d94d0e
idodi33 <idodi33@gmail.com>
2020-06-20 15:52:02 +0300

    Added documentation feature updates.

--- assemblies/assembly_readers/read_recursive.py
+++ assemblies/assembly_readers/read_recursive.py
@@ -3,21 +3,25 @@ from assemblies.assembly_fun import Assembly
 from assemblies.utils import fire_many, revert_changes
 
 
-# TODO: Add documentation!
-
-
 class ReadRecursive:
+    """
+    A class representing a reader that obtains information about an assembly using the 'read' method.
+    The method works by recursively firing areas from the top of the parent tree of the assembly,
+    and examining which neurons were fired.
+    Note: This is the default read driver.
+    """
 
     name = 'default'
 
     @staticmethod
     def read(assembly: Assembly, preserve_brain: bool = False, *, brain: Brain):
         """
-
-        :param assembly: Assembly
-        :param preserve_brain: Do we want to change the brain state or not
-        :param brain:
-        :return:
+        Read the winners from given assembly in given brain recursively using fire_many
+        and return the result.
+        :param assembly: the assembly object
+        :param preserve_brain: a boolean representing whether we want to change the brain state or not
+        :param brain: the brain object
+        :return: the winners as read from the area that we've fired up
         """
         changed_areas = fire_many(brain, assembly.parents, assembly.area, preserve_brain)
         read_value = brain.winners[assembly.area]
--- assemblies/read_driver.py
+++ assemblies/read_driver.py
@@ -2,14 +2,18 @@ import sys
 from pathlib import Path
 import importlib
 import re
-
 from brain import Brain
 
 
-# TODO: Add documentation!
-
-
 class ReadDriver:
+    """
+    An object representing a reader that can use several different implementations for reading
+    from assemblies. The implementation is chosen by the parameter 'reader_name'.
+    An automatic framework collects every class beginning with "Read" from assembly_readers
+    and collects it into the dictionary. The key is the 'name' parameter of the class.
+    :param reader_name: the name of the reader we've chosen to use
+    """
+
     def __init__(self, reader_name):
         self.readers = {}
         # Path to assemblies package
@@ -28,8 +32,21 @@ class ReadDriver:
         self.reader = self.readers[reader_name]
 
     def read(self, assembly, brain: Brain, preserve_brain: bool = False):
+        """
+        Read the winners from given assembly in given brain and return the result.
+        :param assembly: the assembly object
+        :param brain: the brain object
+        :param preserve_brain: a boolean representing whether we want to change the brain state or not
+        :return: the winners as read from the area that we've fired up
+        """
         return self.reader.read(assembly, brain=brain, preserve_brain=preserve_brain)
 
     def update_hook(self, brain: Brain, assembly):
+        """
+        Internal hook for readers to subscribe to, allowing more elaborate read drivers.
+        Called every time an assembly is changed.
+        :param brain: the brain object
+        :param assembly: the assembly object
+        """
         if hasattr(self.reader, 'update_hook'):
             self.reader.update_hook(self, brain, assembly)
